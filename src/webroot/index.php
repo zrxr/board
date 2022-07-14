9b8ade1162b5c5f561bf5a7098027a0e24a1734b
zrxr <zrxr@panda>
2022-07-14 17:20:17 +1000

    Updated src/webroot/index.php

--- src/webroot/index.php
+++ src/webroot/index.php
@@ -21,16 +21,31 @@
         echo '</pre>';
     }
 
+    function env() {
+        $env = array();
+
+        // env var.
+        if( isset($_ENV['BOARD_VAR']) ){
+            $env['var'] = $_ENV['BOARD_VAR'];
+        } elseif ( is_dir('/var/board') && is_writable('/var/board') ) {
+            $env['var'] = '/var/board';
+        } else {
+            $env['var'] = __DIR__.'/../../var';
+        }
+
+        return $env;
+    }
+
     function save($data) {
 
-        // save directory.
-        if( isset($_ENV['BOARD_VAR']) ){
-            $save_dir = $_ENV['BOARD_VAR'] . '/requests';
-        } elseif ( is_dir('/var/board/requests') ) {
-            $save_dir = '/var/board/requests';
-        } else {
-            $save_dir = __DIR__.'/../../var/requests';
-        }
+        // env.
+        $env = env();
+
+        // save dir.
+        $save_dir = $env['var'].'/requests';
+
+        // save dir create.
+        if( ! is_dir($save_dir) ){ mkdir($save_dir); }
 
         // save file.
         $save_file = "request.".time().".json";
@@ -51,14 +66,73 @@
 ?>
 <?php
 
-    // request type.
+    // request type [default].
     $REQUEST_TYPE = "storage";
 
-    // request type [interactive].
+    // request type detect.
     if( $_SERVER['REQUEST_METHOD'] == 'GET' ){
-        if( $_SERVER['QUERY_STRING'] == '' ){
+        if( $_SERVER['QUERY_STRING'] == 'board-request-type-api' ){
+            $REQUEST_TYPE = "api";
+        } elseif( empty($_SERVER['QUERY_STRING']) ){
             $REQUEST_TYPE = "interactive";
+        } else {
+            $REQUEST_TYPE = "storage";
         }
+    }
+
+?>
+<?php
+
+// handle request type [api].
+if( $REQUEST_TYPE == "api" ){
+
+    //files = scandir()
+
+    // env.
+    $env = env();
+
+    // requests dir.
+    $requests_dir = $env['var'].'/requests';
+
+    // requests data.
+    $requests_data = array();
+
+    // requests.
+    foreach( scandir($requests_dir) as $request_file ){
+        if( str_ends_with($request_file, '.json') ){
+        
+            // request json.
+            $request_json = file_get_contents("{$requests_dir}/${request_file}");
+
+            // request json decode.
+            $request_data = json_decode($request_json, true);
+
+            // requests data append request data.
+            $requests_data[] = $request_data;
+
+        }
+    }
+
+    // requests json encode.
+    $requests_json = json_encode($requests_data);
+
+    // requests headers.
+    header('Content-Type: application/json; charset=utf-8');
+
+    // requests response.
+    print($requests_json);
+
+}
+
+?>
+<?php
+
+    // handle request type [interactive].
+    if( $REQUEST_TYPE == "interactive" ){
+
+        // todo ...
+        echo "interactive mode coming soon ...";
+
     }
 
 ?>
@@ -77,16 +151,5 @@
         dump($data);
 
     }
-
-?>
-<?php
-
-// handle request type [interactive].
-if( $REQUEST_TYPE == "interactive" ){
-
-    // todo ...
-    echo "interactive mode coming soon ...";
-
-}
 
 ?>
